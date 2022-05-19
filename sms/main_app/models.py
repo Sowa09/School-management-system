83c7fe4b744e774c1dba5bcc5cae83096ed1b11a
Sowa09 <tsowa09@gmail.com>
2022-05-19 20:29:34 +0200

    Teacher, student, subject models updated

--- sms/main_app/models.py
+++ sms/main_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+GENDER_CHOICES = [
+    ('M', 'Mężczyzna'),
+    ('K', 'Kobieta')
+]
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=16)
+    subject_info = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=16)
+    age = models.IntegerField()
+
+
