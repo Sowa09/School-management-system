8779064681677225ccb5efcce11d6e75c060a45e
Sowa09 <tsowa09@gmail.com>
2022-05-19 20:30:04 +0200

    Login mixin required

--- sms/main_app/views.py
+++ sms/main_app/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
 
 
-class BaseView(View):
+class BaseView(LoginRequiredMixin, View):
+    login_url = '/'
+    redirect_field_name = 'index'
+
     def get(self, request):
         return render(request, '__base__.html')
 
@@ -22,9 +26,12 @@ class LoginView(View):
     def post(self, request):
         username = request.POST['uname']
         password = request.POST['psw']
+        remember_me = request.POST.get('remember', True)
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            if not remember_me:
+                request.session.set_expiry(1000)
             return redirect('index')
         else:
             return HttpResponse('Błędny login lub hasło')
