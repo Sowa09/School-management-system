502e6659ec746b953a23154f015ae20659aa1975
Sowa09 <tsowa09@gmail.com>
2022-05-19 17:56:33 +0200

    base ,login and logout view

--- sms/main_app/views.py
+++ sms/main_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+
+class BaseView(View):
+    def get(self, request):
+        return render(request, '__base__.html')
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, '__login__.html')
+
+    def post(self, request):
+        username = request.POST['uname']
+        password = request.POST['psw']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('Błędny login lub hasło')
