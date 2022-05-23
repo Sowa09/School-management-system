949e80a3089dfa2507cc52a9ffa5d259a7a71c89
Sowa09 <tsowa09@gmail.com>
2022-05-23 20:07:34 +0200

    Models updated

--- sms/main_app/models.py
+++ sms/main_app/models.py
@@ -20,19 +20,30 @@ SCHOOL_SUBJECT = [
     ('Inf', 'Informatyka'),
 ]
 
-
-# class Subject(models.Model):
-#     name = models.CharField(choices=SCHOOL_SUBJECT, max_length=32)
-#
-#     def __str__(self):
-#         return f'{self.name}'
+GRADES = (
+    (1, "1"),
+    (1.5, "1+"),
+    (1.75, "2-"),
+    (2, "2"),
+    (2.5, "2+"),
+    (2.75, "3-"),
+    (3, "3"),
+    (3.5, "3+"),
+    (3.75, "4-"),
+    (4, "4"),
+    (4.5, "4+"),
+    (4.75, "5-"),
+    (5, "5"),
+    (5.5, "5+"),
+    (5.75, "6-"),
+    (6, "6")
+)
 
 
 class Teacher(models.Model):
     first_name = models.CharField(max_length=32, verbose_name='Imię')
     last_name = models.CharField(max_length=32, verbose_name='Nazwisko')
     gender = models.CharField(choices=GENDER_CHOICES, max_length=16, verbose_name='Płeć')
-    subject_info = models.CharField(choices=SCHOOL_SUBJECT, max_length=16, verbose_name='Przedmiot')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -49,6 +60,7 @@ class Student(models.Model):
     last_name = models.CharField(max_length=32, verbose_name='Nazwisko')
     gender = models.CharField(choices=GENDER_CHOICES, max_length=16, verbose_name='Płeć')
     age = models.IntegerField(verbose_name='Wiek')
+    teacher = models.ManyToManyField(Teacher)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.age}'
@@ -70,3 +82,18 @@ class PresenceListForm(ModelForm):
     class Meta:
         model = PresenceList
         fields = '__all__'
+
+
+class Subject(models.Model):
+    name = models.CharField(choices=SCHOOL_SUBJECT, max_length=32, verbose_name='Przedmiot')
+    student = models.ManyToManyField(Student)
+    teacher = models.ManyToManyField(Teacher)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class StudentGrades(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    school_subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    grade = models.FloatField(choices=GRADES)
