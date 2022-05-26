486ca29904aae4f13e6a9490af80c77606e4f29a
Sowa09 <tsowa09@gmail.com>
2022-05-26 18:26:18 +0200

    relations made

--- sms/main_app/models.py
+++ sms/main_app/models.py
@@ -1,9 +1,9 @@
-from django.db import models
-from django.db.models import UniqueConstraint
-from django.forms.models import ModelForm
 import datetime
 
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.forms.models import ModelForm
+from django.utils.timezone import now
 
 GENDER_CHOICES = [
     ('M', 'Mężczyzna'),
@@ -43,32 +43,6 @@ GRADES = (
     (6, "6")
 )
 
-SCHOOL_START_YEAR = (
-    (2015, "2015"),
-    (2016, "2016"),
-    (2017, "2017"),
-    (2018, "2018"),
-    (2019, "2019"),
-    (2020, "2020"),
-    (2021, "2021"),
-    (2022, "2022"),
-    (2023, "2023"),
-    (2024, "2024"),
-    (2025, "2025"),
-    (2026, "2026"),
-    (2027, "2027"),
-    (2028, "2028"),
-    (2029, "2029"),
-    (2030, "2030"),
-    (2031, "2031"),
-    (2032, "2032"),
-    (2033, "2033"),
-    (2034, "2034"),
-    (2035, "2035"),
-    (2036, "2036"),
-
-)
-
 
 def current_year():
     return datetime.date.today().year
@@ -80,8 +54,19 @@ def max_value_current_year(value):
 
 # MODELS
 
+class SchoolClass(models.Model):
+    year = models.PositiveIntegerField(
+        default=now,
+        validators=[MinValueValidator(2020), max_value_current_year]
+    )
+
+    def __str__(self):
+        return f'{str(self.year)}'
+
+
 class Subject(models.Model):
     name = models.CharField(choices=SCHOOL_SUBJECT, max_length=32, verbose_name='Przedmiot')
+    student = models.ManyToManyField('Student')
 
     def __str__(self):
         return f'{self.name}'
@@ -91,6 +76,7 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=32, verbose_name='Imię')
     last_name = models.CharField(max_length=32, verbose_name='Nazwisko')
     gender = models.CharField(choices=GENDER_CHOICES, max_length=16, verbose_name='Płeć')
+    subject_info = models.ManyToManyField(Subject, blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -104,8 +90,7 @@ class TeacherForm(ModelForm):
 
 class Grades(models.Model):
     grade = models.FloatField(choices=GRADES)
-    student = models.ForeignKey('Student', on_delete=models.CASCADE, related_name='student_grades')
-    school_subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+    school_subject = models.ManyToManyField(Subject)
 
 
 class Student(models.Model):
@@ -113,10 +98,8 @@ class Student(models.Model):
     last_name = models.CharField(max_length=32, verbose_name='Nazwisko')
     gender = models.CharField(choices=GENDER_CHOICES, max_length=16, verbose_name='Płeć')
     age = models.IntegerField(verbose_name='Wiek')
-    grades = models.ManyToManyField(Subject, through='Grades')
-    school_class = models.IntegerField(choices=SCHOOL_START_YEAR, default=current_year(),
-                                       verbose_name='Rok rozpoczęcia nauki',
-                                       validators=[MinValueValidator(2016), max_value_current_year])
+    grades = models.ManyToManyField(Grades)
+    school_class = models.ForeignKey(SchoolClass, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.age}'
