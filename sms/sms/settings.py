5a51c59f8fe39fd2a43637f2d525592520b75ab8
Sowa09 <tsowa09@gmail.com>
2022-07-13 18:31:59 +0200

    static files/templates

--- sms/sms/settings.py
+++ sms/sms/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-0=m#!!1c*3re1f97mmh6#hnje(@m!3nek7hcoign-@t!4ti7cj
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sms.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -79,12 +76,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'school_management',
         'HOST': 'localhost',
-        'PASSWORD': 'sowa',
+        'PASSWORD': 'postgres',
         'USER': 'postgres',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -104,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -115,7 +109,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- sms/sms/urls.py
+++ sms/sms/urls.py
@@ -15,19 +15,40 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.templatetags.static import static
+from django.views.generic import TemplateView
 
 from main_app.views import LoginView, LogoutView, BaseView, TeacherListView, TeacherFormView, StudentListView, \
-    StudentFormView, StudentDetailsView
+    StudentFormView, StudentDetailsView, SchoolClassFormView, SchoolClassListView, SchoolClassModify, \
+    StudentClassDetailsView, SubjectFormView, create_user, change_password, DeleteStudentView, DeleteTeacherView, \
+    AddTopicToSubject, GradesFormView, StudentTopicGradeSubjectView
+
 
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'))),
     path('admin/', admin.site.urls),
     path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('index/', BaseView.as_view(), name='index'),
+    path('new_user/', create_user, name='new-user'),
+    path('change_password/', change_password, name='change-password'),
     path('teacher/list', TeacherListView.as_view(), name='teacher-list'),
     path('teacher/add', TeacherFormView.as_view(), name='teacher-add'),
+    path('teacher/delete/<int:pk>', DeleteTeacherView.as_view(template_name='teacher_confirm_delete.html'),
+         name='teacher-delete'),
     path('student/list', StudentListView.as_view(), name='student-list'),
     path('student/add', StudentFormView.as_view(), name='student-add'),
+    path('student/delete/<int:pk>', DeleteStudentView.as_view(template_name='student_confirm_delete.html'),
+         name='student-delete'),
     path('student/<int:student_id>', StudentDetailsView.as_view(), name='student-details'),
+    path('class/add', SchoolClassFormView.as_view(), name='class-add'),
+    path('class/list', SchoolClassListView.as_view(), name='class-list'),
+    path('class/edit/<int:pk>', SchoolClassModify.as_view(template_name='school_class_update_form.html'), name='class-modify'),
+    path('class/details/<int:class_id>', StudentClassDetailsView.as_view(), name='class-details'),
+    path('subject/add', SubjectFormView.as_view(), name='subject-add'),
+    path('topcic/add', AddTopicToSubject.as_view(), name='topic-add'),
+    path('grades/add', GradesFormView.as_view(), name='grades-add'),
+    path('grades/subject/list', StudentTopicGradeSubjectView.as_view(), name='grades-list'),
 ]
